f95dea18c66792dea79a4a6a9595ea7e3710ea6a
nurettin <baneci.anan@gmail.com>
2025-05-30 22:51:38 +0300

    ozellikleri uyguladik

--- logic.py
+++ logic.py
@@ -8,6 +8,8 @@ class Pokemon:
         self.pokemon_trainer = pokemon_trainer
         self.pokemon_number = random.randint(1, 1000)
         self.name = None
+        self.weight=None
+        self.height=None 
         if pokemon_trainer not in Pokemon.pokemons:
             Pokemon.pokemons[pokemon_trainer] = self
         else:
@@ -23,12 +25,36 @@ class Pokemon:
                     return data['forms'][0]['name']  #  Pokémon adını döndürme
                 else:
                     return "Pikachu"  # İstek başarısız olursa varsayılan adı döndürür
+                
+
+    async def load_data(self):
+        # PokeAPI aracılığıyla bir pokémonun adını almak için asenktron metot
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
+        async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
+            async with session.get(url) as response:  # GET isteği gönderme
+                if response.status == 200:
+                    data = await response.json()  # JSON yanıtının alınması ve çözümlenmesi
+                    self.height=data["height"]
+                    self.weight=data["weight"]
+                else:
+                    return None  # İstek başarısız olursa varsayılan adı döndürür
+
+
 
     async def info(self):
         # Pokémon hakkında bilgi döndüren bir metot
         if not self.name:
-            self.name = await self.get_name()  # Henüz yüklenmemişse bir adın geri alınması
-        return f"Pokémonunuzun ismi: {self.name}"  # Pokémon adını içeren dizeyi döndürür
+            self.name = await self.get_name()
+        await self.load_data()  # Henüz yüklenmemişse bir adın geri alınması
+        return f"Pokémonunuzun ismi: {self.name}\nPokemonun boyu:{self.height/10} metre\nPokemonun kilosu:{self.weight/10} kilogram"  # Pokémon adını içeren dizeyi döndürür
 
     async def show_img(self):
-        # PokeAPI aracılığıyla bir pokémon görüntüsünün URL'sini almak için asenktron metot
+       # PokeAPI aracılığıyla bir pokémonun adını almak için asenktron metot
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
+        async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
+            async with session.get(url) as response:  # GET isteği gönderme
+                if response.status == 200:
+                    data = await response.json()  # JSON yanıtının alınması ve çözümlenmesi
+                    return data["sprites"]["front_default"]  #  Pokémon adını döndürme
+                else:
+                    return None  # İstek başarısız olursa varsayılan adı döndürür
